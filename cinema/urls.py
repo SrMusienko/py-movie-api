96b85805eb8f84e347d399f3dd89a15063345335
Sergii Musiienko <vpnmus@ukr.net>
2024-07-29 11:29:01 +0300

    fix solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/cinema/", include("cinema_app.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema_app.urls", namespace="cinema")),
 ]
